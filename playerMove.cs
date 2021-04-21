626fd4a2410cd793264e9af093e6957d0ae6a7b0
Darkhhan <49228208+Darkhhan@users.noreply.github.com>
2021-04-21 22:54:56 +0600

    Add files via upload

---
+++ playerMove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    float speed = 2.5F;
+    float jump = 5f;
+    public string Jump_button = "Jump";
+    void Start()
+    {
+
+    }
+    
+    void Update()
+    {
+      
+        Vector3 horizontalMove= new Vector3(Input.GetAxisRaw("Horizontal")*speed,0f,0f);
+        transform.position += horizontalMove * Time.deltaTime * speed;
+
+        if (Input.GetButtonDown(Jump_button))
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jump), ForceMode2D.Impulse);
+        }
+    }
+
+    
+
+}
