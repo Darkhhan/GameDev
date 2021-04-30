a7fa9f42e8fdcabb9dda0feb7beff130735d7b33
Darkkhan <180107176@stu.sdu.edu.kz>
2021-04-30 14:04:05 +0600

    upload

---
+++ Game/Assets/Enemy.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private float EnemySpeed;
+    [SerializeField]
+    private Vector3[] positions;
+    private int index;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, positions[index], Time.deltaTime * EnemySpeed);
+        Vector3 characterScale = transform.localScale;
+        if (transform.position == positions[index])
+        {
+            if (index == positions.Length - 1)
+            {
+                index = 0;
+                if (positions[index].x > positions[index + 1].x)
+                {
+                    characterScale.x = -1f;
+                }
+                if (positions[index].x < positions[index + 1].x)
+                {
+                    characterScale.x = +1f;
+                }
+            }
+            else
+            {
+                if (positions[index].x > positions[index + 1].x)
+                {
+                    characterScale.x = +1f;
+                }
+                if (positions[index].x < positions[index + 1].x)
+                {
+                    characterScale.x = -1f;
+                }
+                index++;
+
+
+            }
+        }
+        transform.localScale = characterScale;
+    }
+}
--- Game/Assets/playerMove.cs
+++ Game/Assets/playerMove.cs
@@ -2,28 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class playerMove : MonoBehaviour
 {
-    float speed = 2.5F;
-    float jump = 5f;
-    public string Jump_button = "Jump";
+    public float speed = 2.5F;
+    public float jump = 5f;
+    public string JumpButton = "Jump";
     void Start()
     {
 
     }
-    
+
     void Update()
     {
-      
-        Vector3 horizontalMove= new Vector3(Input.GetAxisRaw("Horizontal")*speed,0f,0f);
+
+        Vector3 horizontalMove = new Vector3(Input.GetAxisRaw("Horizontal") * speed, 0f, 0f);
         transform.position += horizontalMove * Time.deltaTime * speed;
 
-        if (Input.GetButtonDown(Jump_button))
+        Vector3 characterScale = transform.localScale;
+        if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            characterScale.x = +1f;
+        }
+        if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            characterScale.x = -1f;
+        }
+        transform.localScale = characterScale;
+        if (Input.GetButtonDown(JumpButton))
         {
             GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jump), ForceMode2D.Impulse);
         }
     }
+    void FixedUpdate()
+    {
 
-    
+    }
+
+
 
 }
